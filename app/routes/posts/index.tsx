c192c8ac3bd53d3c849bdb9656dfb8b54db0f53c
Stan Renia <srenia@idt.pf>
2022-07-14 16:37:25 -1000

    up

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -12,18 +12,18 @@ export const loader = async () => {
 
 export default function Posts() {
   const { posts } = useLoaderData() as LoaderData;
-  console.log('POSTS', posts);
-  
+  console.log("POSTS", posts);
+
   return (
     <main>
       <h1>Posts</h1>
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
-            <Link
-              to={post.slug}
-              className="text-blue-600 underline"
-            >
+            <Link to={post.slug} className="text-blue-600 underline">
               {post.title}
             </Link>
           </li>
